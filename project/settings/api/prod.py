535136ddca79d5dcaae8400c3d876a9823e4c422
Marat Goliaviev <marat@loadimpact.com>
2020-03-12 19:12:41 +0300

    [feat] production secrets

--- project/settings/api/prod.py
+++ project/settings/api/prod.py
@@ -1,25 +1,24 @@
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
+from django_secrets import AwsSecrets
+
 from .._base import *
+
+secret = AwsSecrets(secrets_root='production/test-api/',
+                    region_name='us-east-1').get_secret
 
 DEBUG = False
 
+SECRET_KEY = secret('django_secret_key', 'SECRET_KEY')
+
 sentry_sdk.init(
-    dsn="https://a8eb61375aaa4770a1d218fddfeeadc0@sentry.io/1497944",
+    dsn=secret('sentry', 'dsn'),
     integrations=[DjangoIntegration()]
 )
 
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get("MYSQL_DATABASE", '==missing=='),
-        'PASSWORD': os.environ.get("MYSQL_PASSWORD", '==missing=='),
-        'USER': os.environ.get("MYSQL_USER", '==missing=='),
-        'HOST': os.environ.get("MYSQL_HOST", '==missing=='),
-        'PORT': os.environ.get("MYSQL_PORT", 3306),
-    }
+    'default': secret('django_database')
 }
 
 ROOT_URLCONF = 'urls.prod'
--- project/settings/api/staging.py
+++ project/settings/api/staging.py
@@ -5,20 +5,20 @@ from django_secrets import AwsSecrets
 
 from .._base import *
 
-secrets = AwsSecrets(secrets_root='staging/test-api/',
-                     region_name='eu-west-1')
+secret = AwsSecrets(secrets_root='staging/test-api/',
+                    region_name='eu-west-1').get_secret
 
 DEBUG = False
 
-SECRET_KEY = secrets.get_secret('django_secret_key', 'SECRET_KEY')
+SECRET_KEY = secret('django_secret_key', 'SECRET_KEY')
 
 sentry_sdk.init(
-    dsn=secrets.get_secret('sentry', 'dsn'),
+    dsn=secret('sentry', 'dsn'),
     integrations=[DjangoIntegration()]
 )
 
 DATABASES = {
-    'default': secrets.get_secret('django_database')
+    'default': secret('django_database')
 }
 
 ROOT_URLCONF = 'urls.staging'
