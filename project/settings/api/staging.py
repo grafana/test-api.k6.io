43b56c1fcab057844d27b370c4f4daf270f6851a
Marat Goliaviev <marat@loadimpact.com>
2020-03-12 16:42:25 +0300

    [feat] staging secrets

--- project/settings/api/staging.py
+++ project/settings/api/staging.py
@@ -5,19 +5,19 @@ from django_secrets import AwsSecrets
 
 from .._base import *
 
-secrets = AwsSecrets()
+secrets = AwsSecrets(secrets_root='staging/test-api/')
 
 DEBUG = False
 
-SECRET_KEY = secrets.get_secret('staging/test-api/django_secret_key', 'SECRET_KEY')
+SECRET_KEY = secrets.get_secret('django_secret_key', 'SECRET_KEY')
 
 sentry_sdk.init(
-    dsn=secrets.get_secret('staging/test-api/sentry', 'dsn'),
+    dsn=secrets.get_secret('sentry', 'dsn'),
     integrations=[DjangoIntegration()]
 )
 
 DATABASES = {
-    'default': secrets.get_secret('staging/test-api/django-database')
+    'default': secrets.get_secret('django_database')
 }
 
 ROOT_URLCONF = 'urls.staging'
