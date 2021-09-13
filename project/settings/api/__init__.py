3b209ea9d67cacae87705fc689fc2c85ad646eeb
Artem Charykov <artych.mail@gmail.com>
2021-09-13 11:58:29 +0300

    [tool] k8s

--- project/settings/api/__init__.py
+++ project/settings/api/__init__.py
@@ -7,6 +7,12 @@ elif env == 'production':
     from .production import *
 elif env == 'staging':
     from .staging import *
+
+elif env == 'k8s':
+    # In k8s setup settings come from env vars, specified in the Deployment api object and
+    # configured in k8s ConfigMap or ExternalSecret api objects.
+    # Quering secrets from AWS Secret Manager is the responsibility of the k8s cluster.   
+    from .k8s import *
 elif not env:
     from .default import *
 else:
