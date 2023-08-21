5e11d07d4eb48ce38ce20dd53b18646cca299658
Pepe Cano <825430+ppcano@users.noreply.github.com>
2023-08-22 00:31:18 +0200

    Remove unused `dumpdata.sh`

--- devops/dumpdata.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-# run this from the test_api directory
-
-./project/manage.py dumpdata --indent=4 --exclude=contenttypes --exclude=auth.permission --exclude=admin.logentry --exclude=sessions > devops/fixtures/initial.json
