f0a1bd41ae42606ee97c567dc60635f25c5ee86b
Ayush Goyal <ayushgoyal.iitkgp@gmail.com>
2024-02-12 16:23:01 +0000

    [feat] Add CI job to run tests

--- project/settings/api/__init__.py
+++ project/settings/api/__init__.py
@@ -1,14 +1,14 @@
 from os import environ
 
 env = environ.get("ENVIRONMENT_NAME")
-if env == 'dev':
+if env == "dev":
     from .dev import *
-elif env == 'k8s':
+elif env == "k8s":
     # In k8s setup settings come from env vars, specified in the Deployment api object and
     # configured in k8s ConfigMap or ExternalSecret api objects.
-    # Quering secrets from AWS Secret Manager is the responsibility of the k8s cluster.   
+    # Quering secrets from AWS Secret Manager is the responsibility of the k8s cluster.
     from .k8s import *
 elif not env:
     from .default import *
 else:
-    raise Exception(f'ENVIRONMENT_NAME env incorrect: {env}')
+    raise Exception(f"ENVIRONMENT_NAME env incorrect: {env}")
